529fc43728b4db84dc17fd5855b29fd6be552ba9
depressionmelon07 <107890630+depressionmelon07@users.noreply.github.com>
2022-08-22 12:10:48 +1200

    First version of questions and answers component

---
+++ 04_questions_v1.py
@@ -0,0 +1,75 @@
+score = 0
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+# Question ONE
+print("What colour are bananas?")
+answer_1 = input("1)Red\n2)Orange\n3)Yellow\n4)White")
+if answer_1 == "3" or answer_1.lower() == "yellow":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect")
+
+print("Your score is ", score)
