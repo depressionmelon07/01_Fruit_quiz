7616a9a3adc1a336d18193eb0796e44917e2a46d
depressionmelon07 <107890630+depressionmelon07@users.noreply.github.com>
2022-09-25 23:18:13 +1300

    First test statement decorator

---
+++ 07_statement_decorations_v1.py
@@ -0,0 +1,10 @@
+greeting = "You got 5 points"
+sides = "!" * 3
+
+greeting = "{} {} {}".format(sides, greeting, sides)
+
+top_bottom = "!" * len(greeting)
+
+print(top_bottom)
+print(greeting)
+print(top_bottom)
